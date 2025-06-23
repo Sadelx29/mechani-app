03ccf1f60d2b367351185472d8995514c1f27f81
sadel <s.fortunato@gruporamos.com>
2025-06-22 23:04:27 -0400

    Add Vehicle interface to clientTypes.ts

--- src/features/clients/clientTypes.ts
+++ src/features/clients/clientTypes.ts
@@ -5,3 +5,15 @@ export interface Client {
   email: string;
   isSynced: boolean;
 }
+
+export interface Vehicle {
+  id?: number;
+  clientId: number;
+  brand: string;
+  model: string;
+  year: string;
+  plate: string;
+  color: string;
+  notes?: string;
+  isSynced: boolean;
+}
